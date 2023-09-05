bfe0716d51a38f81c479252e6ecff91fb480e483
Rixon <rixonraj16@gmail.com>
2023-09-06 01:15:57 +0530

    CBHell

---
+++ script.js
@@ -0,0 +1,35 @@
+
+setTimeout(() => {
+    document.getElementById("h1values").innerHTML = "10"; 
+    setTimeout(() => {
+        document.getElementById("h1values").innerHTML = "9"; 
+        setTimeout(() => {
+            document.getElementById("h1values").innerHTML = "8"; 
+            setTimeout(() => {
+                document.getElementById("h1values").innerHTML = "7"; 
+                setTimeout(() => {
+                    document.getElementById("h1values").innerHTML = "6"; 
+                    setTimeout(() => {
+                        document.getElementById("h1values").innerHTML = "5"; 
+                        setTimeout(() => {
+                            document.getElementById("h1values").innerHTML = "4"; 
+                            setTimeout(() => {
+                                document.getElementById("h1values").innerHTML = "3"; 
+                                setTimeout(() => {
+                                    document.getElementById("h1values").innerHTML = "2"; 
+                                    setTimeout(() => {
+                                        document.getElementById("h1values").innerHTML = "1";
+                                        setTimeout(() => {
+                                            document.getElementById("h1values").innerHTML = "Happy Independence Day!"; 
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
